a620c872d8da974e5f05c939b5a04baeb6027cb7
Neal <yhnero.hotmail@gmail.com>
2020-01-28 17:12:10 -0500

    finish adding Access-Control-Allow-Origin to RPC helper.

--- src/rpc/RpcHelper.java
+++ src/rpc/RpcHelper.java
@@ -2,6 +2,7 @@ package rpc;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,13 +13,20 @@ public class RpcHelper {
 	// Writes a JSONArray to http response.
 	public static void writeJsonArray(HttpServletResponse response, JSONArray array) throws IOException {
 		response.setContentType("application/json");
-		response.getWriter().print(array);
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		PrintWriter out = response.getWriter();
+		out.print(array);
+		out.close();
+
 	}
 
 	// Writes a JSONObject to http response.
 	public static void writeJsonObject(HttpServletResponse response, JSONObject obj) throws IOException {
 		response.setContentType("application/json");
-		response.getWriter().print(obj);
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		PrintWriter out = response.getWriter();
+		out.print(obj);
+		out.close();
 	}
 
 	// Parses a JSONObject from http request.
