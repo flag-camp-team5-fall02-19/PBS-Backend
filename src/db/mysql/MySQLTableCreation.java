3cd6862d22c2b0a810971513f1a55c8d83f83b5f
Neal <yhnero.hotmail@gmail.com>
2020-02-13 16:07:43 -0500

    viewOwner, viewOwnerOrder, viewSitterOrder servlet added; multiple entities implemented.

--- src/db/mysql/MySQLTableCreation.java
+++ src/db/mysql/MySQLTableCreation.java
@@ -152,90 +152,90 @@ public class MySQLTableCreation {
 			// Step 4: insert fake owner 1111/3229c1097c00d497a0fd282d586be050
 			sql = "INSERT INTO owners VALUES('1111', '3229c1097c00d497a0fd282d586be050', 'John', 'Smith', '1234567890', 'sun@laioffer.com', '10001', 'New York', 'Fifth Avenue', 'orange cat', 'The most lovely cat in the world')";
 			statement.executeUpdate(sql);
-			// Step 4: insert fake owner 3333/3229c1097c00d497a0fd282d586be050
+			// Step 5: insert fake owner 3333/3229c1097c00d497a0fd282d586be050
 			sql = "INSERT INTO owners VALUES('3333', '3229c1097c00d497a0fd282d586be050', 'Jack', 'Chen', '1234567890', 'jack@gmail.com', '10001', 'New York', 'Fifth Avenue', 'orange cat', 'The most lovely cat in the world')";
 			statement.executeUpdate(sql);
-			// Step 5: insert fake sitter 3333/3229c1097c00d497a0fd282d586be050
+			// Step 6: insert fake sitter 3333/3229c1097c00d497a0fd282d586be050
 			sql = "INSERT INTO sitters VALUES('3333', '3229c1097c00d497a0fd282d586be050', 'Jack', 'Chen', '1234567890', 'jack@gmail.com', '10001', 'New York', 'Fifth Avenue', '99.9', '4.9')";
 			statement.executeUpdate(sql);
-			// Step 6: insert fake sitter 4444/3229c1097c00d497a0fd282d586be050
+			// Step 7: insert fake sitter 4444/3229c1097c00d497a0fd282d586be050
 			sql = "INSERT INTO sitters VALUES('4444', '3229c1097c00d497a0fd282d586be050', 'Alice', 'Jen', '1111111111', 'alice@gmail.com', '15229', 'New West', 'Fifth Avenue', '99.9', '4.9')";
 			statement.executeUpdate(sql);
-			// Step 7: insert fake sitter 5555/3229c1097c00d497a0fd282d586be050
+			// Step 8: insert fake sitter 5555/3229c1097c00d497a0fd282d586be050
 			sql = "INSERT INTO sitters VALUES('5555', '3229c1097c00d497a0fd282d586be050', 'Jensen', 'Wang', '2222222222', 'Jensen@gmail.com', '15139', 'New East', 'Fifth Avenue', '99.9', '4.9')";
 			statement.executeUpdate(sql);
-			// Step 8: insert fake sitter 6666/3229c1097c00d497a0fd282d586be050
+			// Step 9: insert fake sitter 6666/3229c1097c00d497a0fd282d586be050
 			sql = "INSERT INTO sitters VALUES('6666', '3229c1097c00d497a0fd282d586be050', 'Neal', 'Zeng', '3333333333', 'Neal@gmail.com', '15049', 'New South', 'Fifth Avenue', '99.9', '4.9')";
 			statement.executeUpdate(sql);
-			// Step 9: insert fake sitter 7777/3229c1097c00d497a0fd282d586be050
+			// Step 10: insert fake sitter 7777/3229c1097c00d497a0fd282d586be050
 			sql = "INSERT INTO sitters VALUES('7777', '3229c1097c00d497a0fd282d586be050', 'Jill', 'Valentine', '4444444444', 'Jill@gmail.com', '15127', 'New North', 'Fifth Avenue', '99.9', '4.9')";
 			statement.executeUpdate(sql);
-			// Step 10: insert fake sitter post
+			// Step 11: insert fake sitter post
 			sql = "INSERT INTO sitter_posts VALUES('1', '3333', 'fakeurl', 'testcaption')";
 			statement.executeUpdate(sql);
-			// Step 11: insert fake sitter post 2
+			// Step 12: insert fake sitter post 2
 			sql = "INSERT INTO sitter_posts VALUES('2', '3333', 'fakeurl2', 'testcaption2')";
 			statement.executeUpdate(sql);
 			
-			// Step 10: insert fake owner post
+			// Step 13: insert fake owner post
 			sql = "INSERT INTO owner_posts VALUES('1', '1111', 'fakeurl', 'testcaption')";
 			statement.executeUpdate(sql);
-			// Step 11: insert fake owner post 2
+			// Step 14: insert fake owner post 2
 			sql = "INSERT INTO owner_posts VALUES('2', '1111', 'fakeurl2', 'testcaption2')";
 			statement.executeUpdate(sql);
-			// Step 11: insert fake owner post 3
+			// Step 15: insert fake owner post 3
 			sql = "INSERT INTO owner_posts VALUES('3', '3333', 'fakeurl3', 'testcaption3')";
 			statement.executeUpdate(sql);
-			// Step 11: insert fake owner post 4
+			// Step 16: insert fake owner post 4
 			sql = "INSERT INTO owner_posts VALUES('4', '3333', 'fakeurl4', 'testcaption4')";
 			statement.executeUpdate(sql);
 			
-			// Step 12: insert fake request 4444/3229c1097c00d497a0fd282d586be050
+			// Step 17: insert fake request 4444/3229c1097c00d497a0fd282d586be050
 			sql = "INSERT INTO requests VALUES('1', '1111', '4444', TRUE, 'hello', NOW(), NOW())";
 			statement.executeUpdate(sql);
 									
-			// Step 12: insert fake request 4444/3229c1097c00d497a0fd282d586be050
+			// Step 18: insert fake request 4444/3229c1097c00d497a0fd282d586be050
 			sql = "INSERT INTO requests VALUES('2', '3333', '4444', FALSE, 'hi', NOW(), NOW())";
 			statement.executeUpdate(sql);
 									
-			// Step 12: insert fake request 5555/3229c1097c00d497a0fd282d586be050
+			// Step 19: insert fake request 5555/3229c1097c00d497a0fd282d586be050
 			sql = "INSERT INTO requests VALUES('3', '1111', '5555', TRUE, 'hey', NOW(), NOW())";
 			statement.executeUpdate(sql);
 									
-			// Step 12: insert fake request 5555/3229c1097c00d497a0fd282d586be050
+			// Step 20: insert fake request 5555/3229c1097c00d497a0fd282d586be050
 			sql = "INSERT INTO requests VALUES('4', '3333', '5555', FALSE, 'hi there', NOW(), NOW())";
 			statement.executeUpdate(sql);
 			
 			
-			// Step 12: insert fake review 4444/3229c1097c00d497a0fd282d586be050
+			// Step 21: insert fake review 4444/3229c1097c00d497a0fd282d586be050
 			sql = "INSERT INTO orders VALUES('1', '1111', '4444', '1', TRUE, NOW(), NOW())";
 			statement.executeUpdate(sql);
 						
-			// Step 12: insert fake review 4444/3229c1097c00d497a0fd282d586be050
+			// Step 22: insert fake review 4444/3229c1097c00d497a0fd282d586be050
 			sql = "INSERT INTO orders VALUES('2', '3333', '4444', '2', FALSE, NOW(), NOW())";
 			statement.executeUpdate(sql);
 						
-			// Step 12: insert fake review 5555/3229c1097c00d497a0fd282d586be050
+			// Step 23: insert fake review 5555/3229c1097c00d497a0fd282d586be050
 			sql = "INSERT INTO orders VALUES('3', '1111', '5555', '3', FALSE, NOW(), NOW())";
 			statement.executeUpdate(sql);
 						
-			// Step 12: insert fake review 5555/3229c1097c00d497a0fd282d586be050
+			// Step 24: insert fake review 5555/3229c1097c00d497a0fd282d586be050
 			sql = "INSERT INTO orders VALUES('4', '3333', '5555', '4', TRUE, NOW(), NOW())";
 			statement.executeUpdate(sql);
 
-			// Step 12: insert fake review 4444/3229c1097c00d497a0fd282d586be050
+			// Step 25: insert fake review 4444/3229c1097c00d497a0fd282d586be050
 			sql = "INSERT INTO reviews VALUES('1', '1', '1111', '4444', 3, 'good sitter', NOW())";
 			statement.executeUpdate(sql);
 						
-			// Step 13: insert fake owner 4444/3229c1097c00d497a0fd282d586be050
+			// Step 26: insert fake owner 4444/3229c1097c00d497a0fd282d586be050
 			sql = "INSERT INTO reviews VALUES('2', '2', '3333', '4444', 4, 'great sitter', NOW())";
 			statement.executeUpdate(sql);
 			
-			// Step 12: insert fake review 5555/3229c1097c00d497a0fd282d586be050
+			// Step 27: insert fake review 5555/3229c1097c00d497a0fd282d586be050
 			sql = "INSERT INTO reviews VALUES('3', '3', '1111', '5555', 5, 'excellent sitter', NOW())";
 			statement.executeUpdate(sql);
 									
-			// Step 13: insert fake owner 5555/3229c1097c00d497a0fd282d586be050
+			// Step 28: insert fake owner 5555/3229c1097c00d497a0fd282d586be050
 			sql = "INSERT INTO reviews VALUES('4', '4', '3333', '5555', 2, 'poor sitter', NOW())";
 			statement.executeUpdate(sql);
 
