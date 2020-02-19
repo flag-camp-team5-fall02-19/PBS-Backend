76228e867f44cdfe13240c908c648e35fe3685bc
Chloe-123 <jielian4518@gmail.com>
2020-02-19 17:30:35 -0500

    Add comment to confirm request.

--- src/rpc/ConfirmRequest.java
+++ src/rpc/ConfirmRequest.java
@@ -13,7 +13,11 @@ import db.DBConnection;
 import db.DBConnectionFactory;
 
 /**
- * Servlet implementation class Owner Register
+ * It is called when a sitter accepts the request from the owner.
+ * It will: 1) add a new order to the orders table,
+ * 2) change the status of the request into False, which means the request could no longer be accepted,
+ * 3) change the status of other requests sent by the same owner and time conflict with this request into False,
+ * 4) change the status of other requests sent to the same sitter and time conflict with this request into False.
  */
 @WebServlet("/confirmRequest")
 public class ConfirmRequest extends HttpServlet {
